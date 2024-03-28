cf51530ada3cf8dec39e4d373cb2d5228921cd7b
CHIEH-HUNG CHENG <39375337+chengchiehhung@users.noreply.github.com>
2024-03-28 02:38:10 -0400

    commented print

--- extract.py
+++ extract.py
@@ -2,4 +2,4 @@ import requests
 import beautifulsoup4 as bs
 data_source_1 = "https://costellomanagementllc.managebuilding.com/Resident/public/rentals" # Costello management
 res = requests.get(data_source_1).text
-print(res.text)
+# print(res.text)
